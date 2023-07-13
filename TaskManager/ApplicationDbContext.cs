5c70cc59bd3e0b48198ad1798acf1e6a92bce5a6
Alexisdm1985 <alexis.duranmachuca@gmail.com>
2023-07-13 11:29:42 -0400

    Configuracion Identity con EF Core en appSettings

--- TaskManager/ApplicationDbContext.cs
+++ TaskManager/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using TaskManager.Entidades;
 
 namespace TaskManager
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
@@ -17,3 +18,6 @@ namespace TaskManager
 
 
 // Crear nueva entidad: DbSet<Modelo> NombreEntidad {get; set;}
+// Antes esta clase heredaba de DbContext, pero para ocupar Identity con EF Core,
+// se instala una version de Identity que ya trae EF Core, por lo que se cambia DbContext por IdentityDbContext
+// ya que Identity Tambien trae sus propias tablas de usuarios.
--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TaskManager;
 
@@ -6,10 +8,34 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// ///// CONFIGURACION SERVICIO DBCONTEXT EF CORE 7 /////
+/////// --------------------------------------------------------------- Servicios
+///
+// CONFIGURACION SERVICIO DBCONTEXT EF CORE 7
 builder.Services.AddDbContext<ApplicationDbContext>(opciones => opciones.UseSqlServer("name = DefaultConnection"));
 
+// Activacion de servicios de autenticacion
+builder.Services.AddAuthentication();
+
+// Activacion servicio para usar Identity (debe estar activo el servicio de autenticacion de arriba)
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(opciones =>
+{
+    // No necesita confirmacion de cuenta para el registro de usuario
+    opciones.SignIn.RequireConfirmedAccount = false;
+
+}).AddEntityFrameworkStores<ApplicationDbContext>() // Enlaza Identity con EF Core
+.AddDefaultTokenProviders();
+
+// Para usar mis propias clases y enlaces y no los predefinidos por Identity
+builder.Services.PostConfigure<CookieAuthenticationOptions>(IdentityConstants.ApplicationScheme,
+    opciones =>
+    {
+        opciones.LoginPath = "/Usuarios/Login";
+        opciones.AccessDeniedPath = "Usuarios/Login";
+    });
+
 var app = builder.Build();
+////// ------------------------------------------------------------------------------------------------------------------------
+///
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -24,6 +50,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+////// ------------------------------------------------------------------------ Midlewares
+///
+app.UseAuthentication(); // nos permite utilizar la data del usuario autenticado
 app.UseAuthorization();
 
 app.MapControllerRoute(
