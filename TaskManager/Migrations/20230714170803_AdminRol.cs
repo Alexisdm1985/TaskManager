481b723b6ab2613f04ef13c62e1af51aa6a99c2b
Alexisdm1985 <alexis.duranmachuca@gmail.com>
2023-07-14 13:14:56 -0400

    Creacion migracion de AdminRol

---
+++ TaskManager/Migrations/20230714170803_AdminRol.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                IF NOT EXISTS(SELECT Id FROM AspNetRoles WHERE Id = '5629e790-fea6-4138-b8fc-13c7fc3e033a')
+                BEGIN
+	                INSERT AspNetRoles (Id, [Name], [NormalizedName])
+	                VALUES ('5629e790-fea6-4138-b8fc-13c7fc3e033a', 'admin', 'ADMIN')
+                END");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE AspNetRoles WHERE Id = '5629e790-fea6-4138-b8fc-13c7fc3e033a'");
+        }
+    }
+}
