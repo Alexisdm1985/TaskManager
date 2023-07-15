d989f53a848199711b1c0309e6a93d68ab3e6c7e
Alexisdm1985 <alexis.duranmachuca@gmail.com>
2023-07-15 18:28:59 -0400

    Traduccion para archivos JS agregado por medio de constantes globales creados en vista parcial en el Layout principal

---
+++ TaskManager/wwwroot/js/utilidades.js
@@ -0,0 +1,6 @@
+﻿// Se obtienen las constantes desde la vista parcial _TraduccionTextoJs.cshtml
+// el cual contiene el script con la creacion de las constantes globales
+
+function mostrarError() {
+    alert(ALERT_MENSAJE_ERROR);
+}
