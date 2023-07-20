be485c6c2112c337f47236052babe9abeb85650d
Alexisdm1985 <alexis.duranmachuca@gmail.com>
2023-07-20 09:45:08 -0400

    Agregando estilo solamente

--- TaskManager/wwwroot/js/tareas.js
+++ TaskManager/wwwroot/js/tareas.js
@@ -1,6 +1,9 @@
 ﻿function agregarTareas() {
     listadoTareasViewModel.tareas.push(new fnTareaViewModel({ id: 0, titulo: '' }));
 
+    const buttonAudio = new Audio("../Content/sounds/kirby-hi.mp3");
+
+    buttonAudio.play();
     // Focus en el input de titulo de tarea
     $("[name=titulo-tarea]").last().focus();
 
@@ -14,8 +17,11 @@ function manejarFocusOutTituloTarea(tarea) {
         listadoTareasViewModel.tareas.pop();
         return;
     }
-
+    
+    const creacionTareaAudio = new Audio("../Content/sounds/kirby-bonus.mp3")
+    creacionTareaAudio.play();
     // Seteando el id de tarea a distinto a 0 quiere decir que ya no es una nueva
     //tarea por lo que se mostrara entonces en la vista.
     tarea.id(1);
+
 }
