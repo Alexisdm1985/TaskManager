17a4acb48e45584e52a5d82f9a74a2d44a71892e
Alexisdm1985 <alexis.duranmachuca@gmail.com>
2023-07-20 10:25:55 -0400

    Creacion TareasController como controlador estilo Web API

---
+++ TaskManager/Controllers/TareasController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Servicios;
+
+namespace TaskManager.Controllers
+{
+    [Route("api/tareas")]
+    public class TareasController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+        private readonly IServicioUsuarios servicioUsuarios;
+
+        public TareasController(ApplicationDbContext dbContext,
+            IServicioUsuarios servicioUsuarios)
+        {
+            this.dbContext = dbContext;
+            this.servicioUsuarios = servicioUsuarios;
+        }
+    }
+}
---
+++ TaskManager/Servicios/IServicioUsuarios.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Claims;
+
+namespace TaskManager.Servicios
+{
+    public interface IServicioUsuarios
+    {
+        string ObtenerIdUsuarioAutentificado();
+    }
+
+    public class ServicioUsuarios : IServicioUsuarios
+    {
+        private HttpContext httpContext;
+
+        public ServicioUsuarios(IHttpContextAccessor httpContextAccessor)
+        {
+            httpContext = httpContextAccessor.HttpContext;
+        }
+
+        public string ObtenerIdUsuarioAutentificado()
+        {
+
+            if (httpContext.User.Identity.IsAuthenticated)
+            {
+                // "NameIdentifier" corresponde el tipo de Claim ID.
+                var idClaim = httpContext.User.Claims
+                    .Where(claim => claim.Type == ClaimTypes.NameIdentifier)
+                    .FirstOrDefault();
+
+                return idClaim.Value;
+            }
+            else
+            {
+                throw new Exception("El usuario no se encuentra autentificado.");
+            }
+
+        }
+    }
+}
