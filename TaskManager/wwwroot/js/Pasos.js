973ff9c7bf75a9da81731359779ad22a7ae4b047
Alexisdm1985 <alexis.duranmachuca@gmail.com>
2023-07-25 11:40:58 -0400

    UI para crear pasos de tarea agregado en pasos.js y partial _pasos.cshtml

---
+++ TaskManager/wwwroot/js/Pasos.js
@@ -0,0 +1,18 @@
+﻿function manejarClickAgregarPaso() {
+
+    // Condicion para crear un nuevo paso
+    const indice = tareaEditarVM.pasos().findIndex(paso => paso.esNuevoPaso());
+
+    if (indice !== -1) {
+        return; 
+        // Si indice es distinto a -1 significa que encontro un paso nuevo
+        // por lo que se aplica early return para no dejar crear un nuevo paso
+    }
+
+    // Agrega un nuevo paso a tareaEditarVM
+    const nuevoPaso = new pasosTareaVM({ modoEdicion: true, realizado: false });
+    tareaEditarVM.pasos.push(nuevoPaso);
+
+    // Todo elemento con ese nombre y que este visible tendra focus
+    $("[name=txtPasoDescripcion]:visible").focus();
+}
